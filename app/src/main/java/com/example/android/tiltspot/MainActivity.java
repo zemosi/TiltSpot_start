bb442543e63dd5a444705b9d5526e32a19aefe2e
zemosi <ragajiwa.asa@gmail.com>
2021-03-16 14:32:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/tiltspot/MainActivity.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.tiltspot;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
+    // System sensor manager instance.
+    private SensorManager mSensorManager;
+
+    // Accelerometer and magnetometer sensors, as retrieved from the
+    // sensor manager.
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    // TextViews to display current sensor values.
+    private TextView mTextSensorAzimuth;
+    private TextView mTextSensorPitch;
+    private TextView mTextSensorRoll;
+
+    private ImageView mSpotTop;
+    private ImageView mSpotBottom;
+    private ImageView mSpotLeft;
+    private ImageView mSpotRight;
+
+    // Very small values for the accelerometer (on all three axes) should
+    // be interpreted as 0. This value is the amount of acceptable
+    // non-zero drift.
+    private static final float VALUE_DRIFT = 0.05f;
+    private float[] mAccelerometerData=new float[3];
+    private float[] mMagnetometerData=new float[3];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lock the orientation to portrait (for now)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
+        mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
+        mTextSensorRoll = (TextView) findViewById(R.id.value_roll);
+        mSpotTop = findViewById(R.id.spot_top);
+        mSpotBottom = findViewById(R.id.spot_bottom);
+        mSpotLeft = findViewById(R.id.spot_left);
+        mSpotRight = findViewById(R.id.spot_right);
+
+        // Get accelerometer and magnetometer sensors from the sensor manager.
+        // The getDefaultSensor() method returns null if the sensor
+        // is not available on the device.
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    /**
+     * Listeners for the sensors are registered in this callback so that
+     * they can be unregistered in onStop().
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Listeners for the sensors are registered in this callback and
+        // can be unregistered in onStop().
+        //
+        // Check to ensure sensors are available before registering listeners.
+        // Both listeners are registered with a "normal" amount of delay
+        // (SENSOR_DELAY_NORMAL).
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister all sensor listeners in this callback so they don't
+        // continue to use resources when the app is stopped.
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType=sensorEvent.sensor.getType();
+        switch (sensorType){
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerData=sensorEvent.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerData=sensorEvent.values.clone();
+                break;
+        }
+        float[] rotationMatrix=new float[9];
+        boolean rotationOK=SensorManager.getRotationMatrix(rotationMatrix,null,
+                mAccelerometerData, mMagnetometerData);
+        float[] orientationValues=new float[3];
+        if (rotationOK){
+            SensorManager.getOrientation(rotationMatrix,orientationValues);
+        }
+        float azimuth=orientationValues[0];
+        float pitch=orientationValues[1];
+        float roll=orientationValues[2];
+
+        if (Math.abs(pitch)<VALUE_DRIFT){
+            pitch=0;
+        }
+        if (Math.abs(roll)<VALUE_DRIFT){
+            roll=0;
+        }
+
+        mTextSensorAzimuth.setText(String.format("%1$.2f", azimuth));
+        mTextSensorPitch.setText(String.format("%1$.2f", pitch));
+        mTextSensorRoll.setText(String.format("%1$.2f", roll));
+
+        mSpotTop.setAlpha(0f);
+        mSpotBottom.setAlpha(0f);
+        mSpotLeft.setAlpha(0f);
+        mSpotRight.setAlpha(0f);
+
+        if (pitch>0){
+            mSpotBottom.setAlpha(pitch);
+        } else {
+            mSpotTop.setAlpha(Math.abs(pitch));
+        }
+
+        if (roll>0){
+            mSpotLeft.setAlpha(roll);
+        } else {
+            mSpotRight.setAlpha(Math.abs(roll));
+        }
+
+    }
+
+    /**
+     * Must be implemented to satisfy the SensorEventListener interface;
+     * unused in this app.
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+}
